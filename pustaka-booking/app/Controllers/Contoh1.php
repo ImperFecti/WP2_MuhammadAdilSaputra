0bb28edb2f9e5623a5ea2bf1536c00be1b6950a6
ImperFecti <adilm8909@gmail.com>
2024-03-25 11:25:39 +0700

    upload file Contoh1.php

---
+++ pustaka-booking/app/Controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+use App\Controllers\BaseController;
+class Contoh1 extends BaseController
+{
+    public function index()
+    {
+        echo "<h1>PERKENALKAN</h1>";
+        echo "Nama saya Muhammad Adil Saputra
+        Saya tinggal di daerah Pengasinan";
+    }
+}
